01b0c95937835c2abb7bab4ebd642ea93260e911
Your Name <you@example.com>
2023-03-31 22:17:12 -0700

    troubleshooting update code in category-route file

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -10,12 +10,12 @@ router.get('/', (req, res) => {
     attributes: [
       'id',
       'category_name'
-    ], 
+    ],
     include: [{
       model: Product,
       attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
     }]
-  }) .then(dbCategoryData => res.json(dbCategoryData)).catch(err => {
+  }).then(dbCategoryData => res.json(dbCategoryData)).catch(err => {
     console.log(err);
     res.status(500).json(err)
   })
@@ -54,6 +54,22 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update({
+    category_name: req.body.category_name
+  },
+    {
+      where: {
+        id: req.params.id
+      }
+    }).then(dbCategoryData => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: 'No category found' });
+        return;
+      } res.json(dbCategoryData);
+    }).catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
 
 router.delete('/:id', (req, res) => {
